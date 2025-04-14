b9bddbf41b3354c528a110e4714beea05e5949c5
riccardotesta88 <apple.newton.testa@gmail.com>
2025-04-14 13:17:38 +0200

    Aggiunta app per la gestione dei modelli

--- Biblioteca/settings.py
+++ Biblioteca/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'gestione',
 ]
 
 MIDDLEWARE = [
@@ -135,9 +136,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "staticfiles/"
 STATIC_ROOT='staticfiles'
-STATIC_DIR=(os.path.join(BASE_DIR,'static'),)
+STATIC_DIR=os.path.join(BASE_DIR,'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
