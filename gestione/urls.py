fadd4115bcfa3e0ff6d81a95f857ffbffe1f3386
riccardotesta88 <apple.newton.testa@gmail.com>
2025-04-15 14:57:08 +0200

    Aggiunti path per le operazioni

---
+++ gestione/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
+from gestione import views as vi
+
+urlpatterns = [
+    path('', vi.home, name="home"),
+    path('gestione/listalibri/',vi.listalibri, name="lista_libri"),
+    path('gestione/addlibro/',vi.addLibro, name="add_libro"),
+    path('gestione/addautore/',vi.addAutore, name="add_autore"),
+    path('gestione/addeditore',vi.addEditore, name="add_editore"),
+
+    # Gestione percorsi per la gestione dei libri che rimanda al modulo gestione\urls.py
+
+
+]
