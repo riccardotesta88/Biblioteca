c5108c018b7ffd956dd0da94b00e1bef8449c826
riccardotesta88 <apple.newton.testa@gmail.com>
2025-04-17 16:39:00 +0200

    Gestione dell'interfaccia admin

--- gestione/admin.py
+++ gestione/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-
+from django import forms
+import phonenumber_field
 from gestione.models import *
 from phonenumber_field.widgets import PhoneNumberPrefixWidget
 
@@ -9,6 +10,17 @@ from phonenumber_field.widgets import PhoneNumberPrefixWidget
 class LibroAdmin(admin.ModelAdmin):
 
     pass
+
+class EditoreForm(forms.ModelForm):
+    # class Meta:
+    #     widgets = {  # Here
+    #         'phone': PhoneNumberPrefixWidget(region='IT'),
+    #     }
+    pass
 admin.site.register(Libro,LibroAdmin)
 admin.site.register(Autore)
-admin.site.register(Editore)
+
+@admin.register(Editore)
+class ContactAdmin(admin.ModelAdmin):
+    form = EditoreForm
+    list_display = ('ragione_sociale', 'telefono')
