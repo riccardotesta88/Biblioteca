7fbca8243ba6675382f9ae28858a4ab46bc05d9b
riccardotesta88 <apple.newton.testa@gmail.com>
2025-04-17 11:46:26 +0200

    Aggiunto resolver per import file database

--- gestione/urls.py
+++ gestione/urls.py
@@ -1,3 +1,5 @@
+from crypt import methods
+
 from django.contrib import admin
 from django.urls import path, include
 from gestione import views as vi
@@ -7,8 +9,9 @@ urlpatterns = [
     path('gestione/listalibri/',vi.listalibri, name="lista_libri"),
     path('gestione/addlibro/',vi.addLibro, name="add_libro"),
     path('gestione/addautore/',vi.addAutore, name="add_autore"),
-    path('gestione/addeditore',vi.addEditore, name="add_editore"),
+    path('gestione/addeditore/',vi.addEditore, name="add_editore"),
 
+    path('gestione/loadJson/',vi.loadJson, name="load_json"),
     # Gestione percorsi per la gestione dei libri che rimanda al modulo gestione\urls.py
 
 
